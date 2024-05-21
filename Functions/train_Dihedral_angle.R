b345b540610b1533b629548b33f1b9663f2293b3
Choopanian-Peyman <choopanian.p1992@gmail.com>
2024-05-21 12:20:30 +0300

    train dihedral

---
+++ Functions/train_Dihedral_angle.R
@@ -0,0 +1,82 @@
+library(bio3d)
+
+setwd("E:/Proj/structure/report/Phylogenetic Networks for Classification/ferritin/")
+rm(list = setdiff(ls(), lsf.str()))
+#************Initialization***************************
+binw <- 5;
+filaname_chain <- read.delim("~/Desktop/proj/ProteinEnergyProfileSimilarity/Data/Dunbrack/list_with_chainID.txt", header = F)
+Nprotein <- length(filaname_chain[,1])
+freq <- array(0,c(72,72,20))
+inda <- function(Phi, Psi, Binw){
+  indphi <- floor(Phi/Binw)+1
+  indpsi <- floor(Psi/Binw)+1
+  cbind(indphi,indpsi)
+}
+
+for (NP in 1:Nprotein) {
+  #NP <- 4
+  print(NP)
+  pdbname <- tolower(substr(filaname_chain[NP,1],1,4))
+  #********read pdb and select atom section, remove hydrogen atoms********
+  pdb0 <- read.pdb(pdbname)
+  if(nchar(filaname_chain[NP,1])>4 & substr(filaname_chain[NP,1],5,5) != " " & !is.na(match(substr(filaname_chain[NP,1],5,5),unique(pdb0$atom$chain)))){
+    ch <- substr(filaname_chain[NP,1],5,5)
+    sele1 <- atom.select(pdb0,"protein",type = "ATOM", chain=ch)
+  } else if(!is.na(pdb0$atom$chain[1])){
+    for (ich in 1:length(unique(pdb0$atom$chain))) {
+      if(!is.na(atom.select(pdb0,"protein",type = "ATOM", chain=pdb0$atom$chain[ich])$atom[ich])){
+        ch <- pdb0$atom$chain[ich]
+        sele1 <- atom.select(pdb0,"protein",type = "ATOM", chain=pdb0$atom$chain[ich])
+        break
+      }
+    }
+  } else if(is.na(pdb0$atom$chain[1])){
+    ch <- ""
+    sele1 <- atom.select(pdb0,"protein",type = "ATOM")
+  } else if(is.na(atom.select(pdb0,"protein",type = "ATOM", chain=pdb0$atom$chain[ich])$atom[1])){
+    ch <- pdb0$atom$chain[1]
+    sele1 <- atom.select(pdb0,"protein",type = "ATOM")
+  }
+  sele2 <- atom.select(pdb0, "protein", elety=c("H","OXT"),inverse=TRUE)
+  sele3 <- atom.select(pdb0, "noh")
+  sele <- combine.select(sele1, sele2,sele3, operator="AND")
+  pdb1 <- trim.pdb(pdb0,sele)
+  pdb <- pdb1$atom
+  seqres <- pdb[!duplicated(pdb$resno),"resid"]
+  tor <- torsion.pdb(pdb1)
+  phi <- tor$phi; psi <- tor$psi
+  df <- data.frame(phi, psi, seqres)
+  ind <- which(!is.na(df$phi) & !is.na(df$psi))
+  df <- df[ind,]
+  df[df[,1]<0,1] <- df[df[,1]<0,1]+360
+  df[df[,2]<0,2] <- df[df[,2]<0,2]+360
+  am2num <- mapply(amacid2num, as.character(df$seqres))
+  df <- data.frame(df, am2num)
+  indarray <- inda(df$phi,df$psi,binw)
+  df <- data.frame(df, indarray)
+  for (i in 1:dim(df)[1]) {
+    freq[df$indphi[i], df$indpsi[i], df$am2num[i]] <- freq[df$indphi[i], df$indpsi[i], df$am2num[i]]+1
+  }
+}
+
+#**************  save freq by saveRDS(x,file="x.rds") and read by readRDS
+
+RT <- 0.582
+Sigma <- 0.01
+E <- array(0, c(72,72,20))
+fi <- array(0, c(72,72,20))
+Mi <- rep(0,20)
+for (i in 1:20) {
+  Mi[i] <- sum(freq[,,i])
+  fi[,,i] <- freq[,,i]/Mi[i]
+}
+
+fx <- rowSums(freq, dims = 2)/sum(Mi)
+
+for (i in 1:20) {
+  E[,,i] <- RT*log(1+Mi[i]*Sigma)-RT*log(1+Mi[i]*Sigma*(fi[,,i]/fx))
+}
+
+saveRDS(E,"C:/Users/Choopanian/Desktop/New folder/energy_Dihedral_angle.rds")
+
+
