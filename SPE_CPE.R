250f2d15aa963380cc100fc4bd3268d6765fb28c
Mehdi Mirzaie <mirzaie.mhd@gmail.com>
2024-10-30 11:12:40 +0200

    xample

--- SPE_CPE.R
+++ SPE_CPE.R
@@ -1,24 +1,27 @@
+# This script generates the SPE and CPE based on PDBIDs from PDBIDs_Example.csv file.
+# Th outputs are stored in the SPE and CPE data frames.
 # ------------------------------------------------------------
-# ---------- load requirement function -------------
+# ---------- load requirement functions -------------
+# All required functions are available at Functions.R script
 source('Functions.R')
 # ------------------------------------------------------------
 library(bio3d)
 library(geometry)
-exData <- read.csv('Data/example.csv')
+exData <- read.csv('Data/PDBIDs_Example.csv') # This file should include a column that contains the PDBIDs.
 Nprotein <- nrow(exData)
 
-df_SPE <- data.frame(exData,
+SPE <- data.frame(exData,
                      matrix(NA, nrow = Nprotein, ncol = 210))
 
-colnames(df_SPE)[-c(1:2)] <- pair_inter
+colnames(SPE)[-c(1:2)] <- pair_inter
 
-df_CPE <- df_SPE
+CPE <- SPE
 
 
 for (i in 1:Nprotein){
   print(i)
-  pdbname <- substr(df_SPE$pdbID[i],1,4)
-  ch <- substr(df_SPE$pdbID[i],5,5)
+  pdbname <- substr(SPE$pdbID[i],1,4)
+  ch <- substr(SPE$pdbID[i],5,5)
   # ----------------------------------
   # ----- read pdb file ------------
   pdb0 <- read.pdb(pdbname,verbose = FALSE)
@@ -30,22 +33,22 @@ for (i in 1:Nprotein){
   sele <- combine.select(sele1, sele2, sele4, operator="AND")
   new_pdb <- trim.pdb(pdb0, sele,verbose = FALSE)
   # ----------------------------------
-  # ----- contact map ------------
+  # ----- Extract the atomic neighbors ------------
   Net_Frame <- NetworkFrame(PDB = new_pdb)
   # ----------------------------------
-  # ----- SPE ------------
+  # ----- SPE -----------
   energy210 <- Energy_SPE210(Net_Frame, energy_dell_dunbrack)
-  df_SPE[i,-c(1:2)] <- energy210
+  SPE[i,-c(1:2)] <- energy210
   # ---------------------------------------
   # ----------------------------------
-  # ----- extract sequence ------------
+  # ----- extract protein sequence from PDB file ------------
   seq <- new_pdb$seqres
   seq <- seq[names(seq)==ch]
   seq <- paste0(aa321(seq), collapse = '')
   # ----------------------------------
   # ----- CPE ------------
   aa210_p <- Energy_CPE210(seq)
-  df_CPE[i,-c(1:2)] <- aa210_p
+  CPE[i,-c(1:2)] <- aa210_p
 }
 
 # -------------------------------------------------
@@ -54,8 +57,8 @@ library(ggplot2)
 library(ggpubr)
 
 df <- data.frame(exData,
-                 SPE=rowSums(df_SPE[,-c(1:3)]),
-                 CPE=rowSums(df_CPE[,-c(1:3)])/2)
+                 SPE=rowSums(SPE[,-c(1:3)]),
+                 CPE=rowSums(CPE[,-c(1:3)])/2)
 
 ggplot(df, aes(SPE, CPE, col=species))+
   geom_point(size=3, show.legend = F)+
